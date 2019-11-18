437bab02189e3b3b9ff377ac77b08f79f11e31dc
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2019-11-18 10:56:26 -0600

    Updates

---
+++ precache-manifest.73cb5ef17763ec6e1ac9b260ca713ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a20701df324f694338d9f2bc833b9e6",
+    "url": "/magnessconsulting/index.html"
+  },
+  {
+    "revision": "d14c750c9d76768e7ae5",
+    "url": "/magnessconsulting/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "08e30fd7483741d31356",
+    "url": "/magnessconsulting/static/js/2.96e02ede.chunk.js"
+  },
+  {
+    "revision": "d14c750c9d76768e7ae5",
+    "url": "/magnessconsulting/static/js/main.8416c642.chunk.js"
+  },
+  {
+    "revision": "41b85831f6069b437b48",
+    "url": "/magnessconsulting/static/js/runtime-main.9e710824.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/magnessconsulting/static/media/logo.25bf045c.svg"
+  }
+]);
