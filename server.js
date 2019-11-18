637af48ab16db12d1d4d98a4c47206bed6978e78
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2019-11-18 12:25:19 -0600

    adding server

--- server.js
+++ server.js
@@ -1,11 +1,77 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const morgan = require('morgan')
+const session = require('express-session');
+// const passport = require('./server/passport');
 const app = express();
-const port = process.env.PORT || 5000;
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// Define middleware here
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+}
+
+app.use('/magnessconsulting/', express.static(path.join(__dirname, "client/build")));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// Add routes, both API and view
+// app.use();
+
+app.use(
+  session({
+    secret: 'fraggle-rock',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+//DUPLICATE CODE AS ABOVE W/ ONE ADDITIONAL LINE OF CODE
+// app.use(
+//   session({
+//     secret: 'fraggle-rock',
+//     store: new MongoStore({ mongooseConnection: dbConnection }),
+//     resave: false,
+//     saveUninitialized: false
+//   })
+// );
+
+// passport.serializeUser(function(user, done) {
+//   done(null, user._id);
+// });
+ 
+// passport.deserializeUser(function(id, done) {
+//   User.findById(id, function(err, user) {
+//     done(err, user);
+//   });
+// });
+
+app.use( (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  console.log('req.session', req.session);
+  return next();
+});
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/magnessconsulting");
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
